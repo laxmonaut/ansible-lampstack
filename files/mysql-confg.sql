f5c609491b8cdfc67aeff5ca94db5b8a70d07e45
Kaushik P <kaushikpk42@gmail.com>
2022-08-14 23:29:33 +0530

    Add files via upload

---
+++ files/mysql-confg.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE ecomdb;
+CREATE USER 'ecomuser'@'localhost' IDENTIFIED BY 'ecompassword';
+CREATE USER 'ecomuser'@'%' IDENTIFIED BY 'ecompassword';
+GRANT ALL PRIVILEGES ON *.* TO 'ecomuser'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'ecomuser'@'%';
+FLUSH PRIVILEGES;
